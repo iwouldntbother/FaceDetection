cd4cf3ee9496fdbbfc48cc18305650272cf8eefd
WWSTWD <65234242+wwstwd@users.noreply.github.com>
2022-05-06 12:20:54 +0100

    Added more quiz questions

--- js/quizQuestions.js
+++ js/quizQuestions.js
@@ -13,5 +13,15 @@ const questions = [
     questionTitle: 'In 2019 a facial recogniton databased leaked, how many records were from a single day?',
     answers: ['~18.9 Million','~6.8 Million','~3.0 Billion','~8.8 Thousand'],
     correctAnswer: 1
+  },
+  {
+    questionTitle: 'How much is the facial recognition market value expected to reach by 2025?',
+    answers: ['$8.9 Million','$8.5 Billion','$30.0 Billion','$8.8 Billion'],
+    correctAnswer: 1
+  },
+  {
+    questionTitle: 'How many people wrongly identified by police facial recognition in the UK?',
+    answers: ['~3,000','~13,000','~2,000,000','~6,000'],
+    correctAnswer: 0
   }
 ]
